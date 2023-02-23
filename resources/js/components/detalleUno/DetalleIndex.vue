9ca882c3902f9d4738a90d50d731599a47b50ca3
cacp1991 <ccalderonperez2011@gmail.com>
2023-02-22 21:58:33 -0500

    Arreglo detalle producto

--- resources/js/components/detalleUno/DetalleIndex.vue
+++ resources/js/components/detalleUno/DetalleIndex.vue
@@ -1,82 +1,86 @@
 <template>
+	<div class="card m-5 ">
+		<div class="card-header bg-warning text-white">Detalle del articulo</div>
+		<div class="card-body ">
 
-    <div class="card m-5" >
-  <div class="card-header bg-warning  text-white ">
-   Detalle del articulo
-  </div>
-  <div class="card-body">
-    <section class="d-flex justify-content-center flex-wrap">
-		<div class="card m-5" style="width: 14rem">
+			<section class="d-flex justify-content-center flex-wrap">
+				<div class="card m-5" style="width: 14rem">
+					<img
+						:src="`/storage/images/${product.image}`"
+						alt=""
+						class="card-img-top "
+						width="100"
+						height="160"
+						v-if="product.image"
 
-			<img
-				:src="`/storage/images/${product.image}`"
-				alt=""
-				class="card-img-top"
-				width="100"
-				height="160"
-				v-if="product.image"
-			/>
+					/>
 
-			<img src="electrodomesticos.jpeg" class="card-img-top" alt="producto" v-else />
+					<img src="electrodomesticos.jpeg" class="card-img-top" alt="producto" v-else />
+				</div>
+			</section>
 
-        </div>
-	</section>
+			<h2 class="card-title">{{ product.name }}</h2>
+			<text class="card-text">{{ product.description }}</text>
+			<h3 class="card-price">Precio: ${{ product.price }}</h3>
+			<H5 class="card-stock"> {{ product.stock }} unidades disponibles</H5>
+            <div><input type="number" v-model="product.stock" /></div>
+			<hr />
+			<a class="btn btn-primary" @click="add(product)">añadir a carrito</a><br />
+		</div>
+	</div>
 
-    <h2 class="card-title">{{ product.name }}</h2>
-    <text class="card-text">{{ product.description }}</text>
-    <h3 class="card-price">Precio: ${{ product.price }}</h3>
-    <H5 class="card-stock"> {{ product.stock }} unidades disponibles</H5>
-    <input type="number" id=""/>
-    <hr>
-    <a class="btn btn-primary" @click="add(product)">añadir a carrito</a><br>
+	<div class="card-body d-flex justify-content-center flex-wrap">
+		<H5 class="card-name"></H5>
+		<H5 class="card-description"></H5>
 
-  </div>
-</div>
-
-			<div class="card-body d-flex justify-content-center flex-wrap">
-				<H5 class="card-name"></H5>
-				<H5 class="card-description"></H5>
-
-
-
-			</div>
+	</div>
 
 </template>
 
 <script>
- import {getData, saveData} from '../../localStorage'
+	import { getData, saveData } from '../../localStorage'
 	export default {
-		props: ['product','user'],
-        created(){
-            console.log(this.product)
-        },
+		props: ['product', 'user'],
+        data() {
+			return {
+				stock: 0
+			}
+		},
+		created() {
+			console.log(this.product)
+		},
 		methods: {
-
-            async add (product) {
-                if  (!this.user){
-                    await swal.fire({
+			async add(product) {
+				if (!this.user) {
+					await swal.fire({
 						icon: 'error',
 						title: 'Lo siento...',
 						text: 'Inicia sesion para continuar con tu compra'
 					})
-                    window.location.replace("/login");
+					window.location.replace('/login')
+				} else {
+					console.log(product)
+					let data = getData(this.user.id)
+					if (!data) data = []
+					data = [...data, product]
+					console.log(data)
+					saveData(this.user.id, data)
 
-                } else{
-
-                console.log(product);
-                let data = getData (this.user.id)
-                if (!data) data = []
-                data = [...data,product]
-                console.log(data);
-                saveData (this.user.id, data)
-
-                 swal.fire({
+					swal.fire({
 						icon: 'success',
 						title: 'Felicidades',
 						text: 'producto añadido al carrito de compras'
 					})
-                }
-            }
-        }
+				}
+			},
+            añadirProducto() {
+				this.stock = 0
+				console.log(this.products)
+				this.products.map(productoslistados => {
+					this.stock += productoslistados.stock * productoslistados.price
+				})
+				console.log(this.total)
+			}
+		}
 	}
 </script>
--- resources/js/components/kart/kartIndex.vue
+++ resources/js/components/kart/kartIndex.vue
@@ -1,11 +1,11 @@
 <template>
-	<section class="d-flex justify-content-center flex-wrap m-5">
+	<section class="d-flex justify-content-center flex-wrap m-5 ">
 		<nav aria-label="breadcrumb">
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="/">Inicio</a></li>
 			</ol>
 		</nav>
-		<table class="table" id="">
+		<table class="table " id="">
 			<thead>
 				<tr>
 					<th>image</th>
@@ -17,12 +17,12 @@
 			</thead>
 			<tbody>
 				<tr v-for="(product, index) in products" :key="index">
-					<section class="d-flex justify-content-center flex-wrap">
+					<section class="d-flex justify-content-center flex-wrap ">
 						<div class="card m-5" style="width: 14rem">
 							<img
 								:src="`/storage/images/${product.image}`"
 								alt=""
-								class="card-img-top"
+								class="card-img-top "
 								width="100"
 								height="160"
 								v-if="product.image"
@@ -32,7 +32,9 @@
 
 					<td>{{ product.name }}</td>
 					<td>{{ product.description }}</td>
-					<td ><strong>$ {{ product.price }}</strong></td>
+					<td>
+						<strong>$ {{ product.price }}</strong>
+					</td>
 					<td><input v-model="product.stock" /></td>
 					<td>
 						<button
@@ -47,8 +49,8 @@
 				</tr>
 			</tbody>
 		</table>
-		<strong>Total $</strong><p >{{ total }}</p>
-
+		<strong>Total $</strong>
+		<p>{{ total }}</p>
 	</section>
 </template>
 
@@ -58,32 +60,21 @@
 		props: ['user'],
 		data() {
 			return {
-                suma:{},
+				suma: {},
 				products: [],
-                cesta: '',
-				total: 0
+				cesta: '',
+				total: 0,
+                stock:0
 			}
 		},
 		created() {
 			this.index()
-
 		},
-		//  para la cesta del carrito
-		// computed: {
-		// 	cestaTotal: function () {
-        //         this.total = 0
-				// let suma = 0
-				// for (key in this.cesta) {
-				// 	suma = suma + this.cesta[key].product.price * this.cesta[key].product.stock
-				// }
-				// return suma
-			// }
-		// },
 
 		methods: {
 			index() {
 				this.products = this.getProduct()
-                this.cestaTotal()
+				this.cestaTotal()
 			},
 			getProduct() {
 				return getData(this.user.id) ?? []
@@ -97,26 +88,14 @@
 				this.index()
 				this.$forceUpdate() //forzar
 			},
-            cestaTotal()
-            {
-                this.total = 0
-                console.log(this.products)
-                this.products.map(e =>{
-                    this.total += e.stock * e.price
-
-                })
-                console.log(this.total)
-
-                // this.cesta = 0
-                // console.log(this.products)
-                // this.products.map(e =>{
-                //     this.cesta += e.stock * e.price
-
-                // })
-                // console.log(this.cesta)
-
-
-            }
+			cestaTotal() {
+				this.total = 0
+				console.log(this.products)
+				this.products.map(productoslistados => {
+					this.total += productoslistados.stock * productoslistados.price
+				})
+				console.log(this.total)
+			}
 		}
 	}
 </script>
